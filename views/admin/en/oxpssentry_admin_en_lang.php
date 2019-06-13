ee9de6bdbfa1c95b159c0fb2ee22e34c982f7ef6
alfredbez <alfred.bez@oxid-esales.com>
2019-06-13 16:20:17 +0200

    feature: added proxy option

--- views/admin/en/oxpssentry_admin_en_lang.php
+++ views/admin/en/oxpssentry_admin_en_lang.php
@@ -9,6 +9,7 @@ $aLang = [
     'SHOP_MODULE_GROUP_main'                   => 'General',
     'SHOP_MODULE_oxpsSentryEnvirnoment'        => 'Environment Name',
     'SHOP_MODULE_oxpsSentryPhpUrl'             => 'PHP-API Url',
+    'SHOP_MODULE_oxpsSentryProxy'              => 'Proxy',
     'SHOP_MODULE_oxpsSentryJsUrl'              => 'JS-API Url',
     'SHOP_MODULE_oxpsSentryLogLevel'           => 'Log-Level, to send logs directly to sentry',
     'SHOP_MODULE_oxpsSentryLogLevel_none'      => 'none',
